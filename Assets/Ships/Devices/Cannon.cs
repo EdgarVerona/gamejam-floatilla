3b2baef259e50565c2a7dcf3d431da76a5c56a19
Alex Loret de Mola <EdgarVerona@gmail.com>
2021-02-16 23:41:00 -0800

    First pass cannon, engine data, and some refactoring

---
+++ Assets/Ships/Devices/Cannon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    [SerializeField]
+    int Damage = 1;
+
+    [SerializeField]
+    float ReloadTimeInSeconds = 3.0f;
+
+    [SerializeField]
+    float ProjectileSpeed = 5.0f;
+
+    [SerializeField]
+    float ProjectileRange = 20.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ships/Devices/Engine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    [SerializeField]
+    float ForcePerSecond = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
