b9fe60728a019f3c26f7ff5d6e5d2c80e0f2c185
Alex Loret de Mola <EdgarVerona@gmail.com>
2021-02-18 00:02:40 -0800

    Editor side of adding devices to hulls

--- Assets/Ships/Hulls/Hull.cs
+++ Assets/Ships/Hulls/Hull.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Hull : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject TopDevice;
+    [SerializeField]
+    public GameObject BottomDevice;
+    [SerializeField]
+    public GameObject LeftDevice;
+    [SerializeField]
+    public GameObject RightDevice;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Ships/Hulls/HullGridEditor.cs
+++ Assets/Ships/Hulls/HullGridEditor.cs
@@ -13,9 +13,10 @@ using UnityEngine;
 /// </summary>
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Hull))]
 public class HullGridEditor : MonoBehaviour
 {
-	Vector2Int _lastKnownPosition;
+	private Vector2Int _lastKnownPosition;
 
 	private void Start()
 	{
@@ -32,31 +33,65 @@ public class HullGridEditor : MonoBehaviour
 			&& prefabStage.mode == PrefabStage.Mode.InIsolation
 			&& prefabStage.scene.path == this.gameObject.scene.path;
 #endif
-
 		// Only allow for this editing in prefab mode
 		if (Application.isEditor && !Application.isPlaying)
 		{
-			// Find the position that the Grid thinks our current position in the world
-			// should equate to, and assign the waypoint to that new grid position.
-			// This makes sure that, if the user is trying to move the cube in the world
-			// to a spot that would equate to a new Grid position, that the new Grid position
-			// will actually be stored successfully for this cube.
-			Vector2Int newGridPosition = HullGridAdapter.GetGridPosition(
-				this.transform.localPosition.x,
-				this.transform.localPosition.z);
-
-			// Now that the grid position is updated, reset our position in the world
-			// to match whatever the Grid thinks it should be.  This "Snaps" the in-world Cube
-			// to the grid's alignment, making sure it can't be placed in an illegal "partway" point
-			// in the world, or be rotated illegally.
-			Vector2 worldCoords = HullGridAdapter.GetWorldPosition(newGridPosition);
-			this.transform.localPosition = new Vector3(worldCoords.x, 0.0f, worldCoords.y);
-			this.transform.localRotation = Quaternion.identity;
-
-			_lastKnownPosition = newGridPosition;
-
-			UpdateLabel();
+			AffixToGrid();
 		}
+	}
+
+
+	private void OnDrawGizmos()
+	{
+		Hull hull = this.GetComponent<Hull>();
+
+		var bounds = MathUtilities.GetMaxBounds(this.gameObject);
+
+		Gizmos.color = Color.blue;
+		DrawGizmoForDevice(bounds, hull.TopDevice, Vector3.forward);
+		DrawGizmoForDevice(bounds, hull.BottomDevice, Vector3.back);
+		DrawGizmoForDevice(bounds, hull.LeftDevice, Vector3.left);
+		DrawGizmoForDevice(bounds, hull.RightDevice, Vector3.right);
+	}
+
+	private void DrawGizmoForDevice(Bounds bounds, GameObject device, Vector3 direction)
+	{
+		if (device != null)
+		{
+			Vector3 destination = GetGizmoDestVector(bounds, direction);
+			
+			Gizmos.DrawLine(bounds.center + new Vector3(0.0f, bounds.extents.y, 0.0f), destination);
+			Gizmos.DrawIcon(destination, device.name + ".png", false);
+		}
+	}
+
+	private Vector3 GetGizmoDestVector(Bounds bounds, Vector3 direction)
+	{
+		return bounds.center + (direction * bounds.extents.z * 0.75f) + new Vector3(0.0f, bounds.extents.y * 1.1f, 0.0f);
+	}
+
+	private void AffixToGrid()
+	{
+		// Find the position that the Grid thinks our current position in the world
+		// should equate to, and assign the waypoint to that new grid position.
+		// This makes sure that, if the user is trying to move the cube in the world
+		// to a spot that would equate to a new Grid position, that the new Grid position
+		// will actually be stored successfully for this cube.
+		Vector2Int newGridPosition = HullGridAdapter.GetGridPosition(
+			this.transform.localPosition.x,
+			this.transform.localPosition.z);
+
+		// Now that the grid position is updated, reset our position in the world
+		// to match whatever the Grid thinks it should be.  This "Snaps" the in-world Cube
+		// to the grid's alignment, making sure it can't be placed in an illegal "partway" point
+		// in the world, or be rotated illegally.
+		Vector2 worldCoords = HullGridAdapter.GetWorldPosition(newGridPosition);
+		this.transform.localPosition = new Vector3(worldCoords.x, 0.0f, worldCoords.y);
+		this.transform.localRotation = Quaternion.identity;
+
+		_lastKnownPosition = newGridPosition;
+
+		UpdateLabel();
 	}
 
 	private void UpdateLabel()
